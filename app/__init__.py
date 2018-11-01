75d655f9d8d848a63b7e27bed8605bed834dd1df
moisessantosf <moisessantosf@gmail.com>
2018-11-01 13:59:31 -0300

    Merge branch 'master' of C:\Users\Santos\PycharmProjects\python-flask-dashboard with conflicts.

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,15 @@ from flask import Flask
 from flask_caching import Cache
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from flask_login import LoginManager
 
 dashboard = Flask(__name__)
 dashboard.config.from_object(Config)
 db = SQLAlchemy(dashboard)
 cache = Cache(dashboard)
+login = LoginManager(dashboard)
 
 import app.routes
 
 if __name__ == "__main__":
-    dashboard.run(host= '192.168.25.24', debug=False, port=5000)
+    dashboard.run(host='192.168.25.20', debug=False, port=5000)
